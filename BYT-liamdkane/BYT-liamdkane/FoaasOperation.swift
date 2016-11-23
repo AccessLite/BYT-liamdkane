973edc0cb803b1b371fe04801dd0b37e69742d08
Louis Tur <louis.tur@gmail.com>
2016-11-23 01:43:16 -0500

    code comments

--- BYT-liamdkane/BYT-liamdkane/FoaasOperation.swift
+++ BYT-liamdkane/BYT-liamdkane/FoaasOperation.swift
@@ -12,6 +12,7 @@ class FoaasOperation: JSONConvertible, DataConvertible {
     let name: String
     let url: String
     //TODO: Clarify if Louis wants to nest objects in this property or nah
+    // if you mean make this [FoaasField]: yes
     let fields: [[String: AnyObject]]
     
     init(name: String, url: String, fields: [[String: AnyObject]]) {
@@ -23,14 +24,15 @@ class FoaasOperation: JSONConvertible, DataConvertible {
     //MARK: - Data Convertible
     
     func toData() throws -> Data {
-        let data = try JSONSerialization.data(withJSONObject: self.toJson(), options: [])
-        return data
+        // nice!! I was looking for this specific solution (but as a 1-liner)
+        return try JSONSerialization.data(withJSONObject: self.toJson(), options: [])
     }
     
     required convenience init?(data: Data) {
         do {
             let validJSON = try JSONSerialization.jsonObject(with: data, options: [])
-            guard let validObject = validJSON as? [String: AnyObject] else {return nil}
+            // always have {} bookmarked by (at least) a space
+            guard let validObject = validJSON as? [String: AnyObject] else { return nil }
             self.init(json: validObject)
         }
         catch {
@@ -42,12 +44,11 @@ class FoaasOperation: JSONConvertible, DataConvertible {
     //MARK: - JSON Convertible
     
     func toJson() -> [String : AnyObject] {
-        let json: [String: AnyObject] = [
+        return [
             "name" : self.name as AnyObject,
             "url" : self.url as AnyObject,
-            "fields" : self.fields as AnyObject
+            "fields" : self.fields as AnyObject // this will need some changes after you make fields [FoaasField]
         ]
-        return json
     }
     
     required convenience init?(json: [String : AnyObject]) {
@@ -57,6 +58,7 @@ class FoaasOperation: JSONConvertible, DataConvertible {
             let fields = json["fields"] as? [[String: AnyObject]] else {
                 return nil
         }
+        // convert fields to [FoaasField]
         self.init(name: name, url: url, fields: fields)
     }
 }
